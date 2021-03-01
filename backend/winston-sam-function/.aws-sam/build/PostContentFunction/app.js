d5d8f9273432d97e394368704d2820d0d0a14834
Alex Hladun <alexhladun@gmail.com>
2021-03-01 11:03:29 -0700

    Initial commit

---
+++ backend/winston-sam-function/.aws-sam/build/PostContentFunction/app.js
@@ -0,0 +1,68 @@
+var AWS = require("aws-sdk");
+
+var dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.lambdaHandler = async (event, context) => {
+  let response = {
+    isBase64Encoded: false,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+  };
+  try {
+    console.log(
+      "🚀 ~ file: app.js ~ line 9 ~ exports.lambdaHandler= ~ event",
+      JSON.stringify(event)
+    );
+
+    let user = event.requestContext.authorizer.claims["cognito:username"];
+    console.log(
+      "🚀 ~ file: app.js ~ line 15 ~ exports.lambdaHandler= ~ user",
+      user
+    );
+
+    const body = JSON.parse(event.body);
+    const uri = body.uri;
+    console.log(
+      "🚀 ~ file: app.js ~ line 18 ~ exports.lambdaHandler= ~ uri",
+      uri
+    );
+    const contentType = body.contentType;
+    console.log(
+      "🚀 ~ file: app.js ~ line 20 ~ exports.lambdaHandler= ~ contentType",
+      contentType
+    );
+    const text = body.text;
+    console.log(
+      "🚀 ~ file: app.js ~ line 22 ~ exports.lambdaHandler= ~ text",
+      text
+    );
+
+    await dynamo
+      .put({
+        TableName: "winston",
+        Item: {
+          PK: `USER#${user}`,
+          SK: `ROUND#${user}${Date.now()}`,
+          timestamp: Date.now(),
+          ImageURI: uri,
+          ContentType: contentType,
+          text: text,
+          username: user
+        }
+      })
+      .promise();
+    console.log("SUCCESS WITH DYNAMO");
+    response.statusCode = 200;
+    return response;
+  } catch (err) {
+    console.log("errrrr", err);
+    response.statusCode = 500;
+
+    response.body = {
+      error: `something happened, ${err}`
+    };
+    return response;
+  }
+};
